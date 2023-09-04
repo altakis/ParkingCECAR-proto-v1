e32f168b01d77705e44aa3a8bd15c9bf227b8871
ReadingShades <42413498+ReadingShades@users.noreply.github.com>
2023-09-03 21:06:19 -0500

    Feat added detection save feature to system

--- app.py
+++ app.py
@@ -1,138 +1,9 @@
 import io
 import gradio as gr
-import matplotlib.pyplot as plt
-import requests, validators
-import torch
-from PIL import Image
-from transformers import (
-    AutoFeatureExtractor,
-    YolosForObjectDetection,
-    DetrForObjectDetection,
-)
+from license_detector import license_detector
+from FileManagerUtil import FileManagerUtil
 
-
-# import os
-# os.environ["KMP_DUPLICATE_LIB_OK"]="TRUE"
-
-# colors for visualization
-COLORS = [
-    [0.000, 0.447, 0.741],
-    [0.850, 0.325, 0.098],
-    [0.929, 0.694, 0.125],
-    [0.494, 0.184, 0.556],
-    [0.466, 0.674, 0.188],
-    [0.301, 0.745, 0.933],
-]
-
-
-def make_prediction(img, feature_extractor, model):
-    inputs = feature_extractor(img, return_tensors="pt")
-    outputs = model(**inputs)
-    img_size = torch.tensor([tuple(reversed(img.size))])
-    processed_outputs = feature_extractor.post_process(outputs, img_size)
-    return processed_outputs[0]
-
-
-def fig2img(fig):
-    buf = io.BytesIO()
-    fig.savefig(buf)
-    buf.seek(0)
-    pil_img = Image.open(buf)
-    basewidth = 750
-    wpercent = basewidth / float(pil_img.size[0])
-    hsize = int((float(pil_img.size[1]) * float(wpercent)))
-    img = pil_img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
-    return img
-
-
-def visualize_prediction(img, output_dict, threshold=0.5, id2label=None):
-    keep = output_dict["scores"] > threshold
-    boxes = output_dict["boxes"][keep].tolist()
-    scores = output_dict["scores"][keep].tolist()
-    labels = output_dict["labels"][keep].tolist()
-
-    if id2label is not None:
-        labels = [id2label[x] for x in labels]
-
-    plt.figure(figsize=(50, 50))
-    plt.imshow(img)
-    ax = plt.gca()
-    colors = COLORS * 100
-    for score, (xmin, ymin, xmax, ymax), label, color in zip(
-        scores, boxes, labels, colors
-    ):
-        if label == "license-plates":
-            ax.add_patch(
-                plt.Rectangle(
-                    (xmin, ymin),
-                    xmax - xmin,
-                    ymax - ymin,
-                    fill=False,
-                    color=color,
-                    linewidth=10,
-                )
-            )
-            ax.text(
-                xmin,
-                ymin,
-                f"{label}: {score:0.2f}",
-                fontsize=60,
-                bbox=dict(facecolor="yellow", alpha=0.8),
-            )
-    plt.axis("off")
-    return fig2img(plt.gcf())
-
-
-def reduce_visual_to_license(img, output_dict, threshold=0.5, id2label=None):
-    keep = output_dict["scores"] > threshold
-    boxes = output_dict["boxes"][keep].tolist()
-    scores = output_dict["scores"][keep].tolist()
-    labels = output_dict["labels"][keep].tolist()
-
-    crop_img = img.crop(*boxes)
-    return crop_img
-
-
-def get_original_image(url_input):
-    if validators.url(url_input):
-        image = Image.open(requests.get(url_input, stream=True).raw)
-
-        return image
-
-
-def detect_objects(model_name, url_input, image_input, webcam_input, threshold):
-    # Extract model and feature extractor
-    feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
-
-    if "yolos" in model_name:
-        model = YolosForObjectDetection.from_pretrained(model_name)
-    elif "detr" in model_name:
-        model = DetrForObjectDetection.from_pretrained(model_name)
-
-    if validators.url(url_input):
-        image = get_original_image(url_input)
-
-    elif image_input:
-        image = image_input
-
-    elif webcam_input:
-        image = webcam_input
-        optional = True
-
-    # Make prediction
-    processed_outputs = make_prediction(image, feature_extractor, model)
-
-    # Visualize prediction
-    viz_img = visualize_prediction(
-        image, processed_outputs, threshold, model.config.id2label
-    )
-
-    if optional:
-        crop_img = reduce_visual_to_license(
-            image, processed_outputs, threshold, model.config.id2label
-        )
-        return viz_img, crop_img
-    return viz_img
+detector = license_detector()
 
 
 def set_example_image(example: list) -> dict:
@@ -141,7 +12,7 @@ def set_example_image(example: list) -> dict:
 
 def set_example_url(example: list) -> dict:
     return gr.Textbox.update(value=example[0]), gr.Image.update(
-        value=get_original_image(example[0])
+        value=detector.get_original_image(example[0])
     )
 
 
@@ -165,6 +36,23 @@ with demo:
     )
 
     with gr.Tabs():
+        with gr.TabItem("WebCam"):
+            with gr.Row():
+                web_input = gr.Image(
+                    source="webcam", type="pil", shape=(750, 750), streaming=True
+                )
+                img_output_from_webcam = gr.Image(shape=(750, 750))
+                with gr.Column():
+                    img_crop_from_webcam = gr.Image(shape=(500, 500))
+                    gr.TextArea(
+                        interactive=False,
+                        label="license_text",
+                        info="license character data",
+                        lines=2,
+                    )
+
+            cam_but = gr.Button("Detect")
+
         with gr.TabItem("Image URL"):
             with gr.Row():
                 with gr.Column():
@@ -172,9 +60,20 @@ with demo:
                         lines=2, label="Enter valid image URL here.."
                     )
                     original_image = gr.Image(shape=(750, 750))
-                    url_input.change(get_original_image, url_input, original_image)
+                    url_input.change(
+                        detector.get_original_image, url_input, original_image
+                    )
                 with gr.Column():
                     img_output_from_url = gr.Image(shape=(750, 750))
+            with gr.Row():
+                with gr.Column():
+                    img_crop_from_url = gr.Image(shape=(500, 500))
+                    gr.TextArea(
+                        interactive=False,
+                        label="license_text",
+                        info="license character data",
+                        lines=2,
+                    )
 
             with gr.Row():
                 example_url = gr.Examples(
@@ -187,6 +86,15 @@ with demo:
             with gr.Row():
                 img_input = gr.Image(type="pil", shape=(750, 750))
                 img_output_from_upload = gr.Image(shape=(750, 750))
+            with gr.Row():
+                with gr.Column():
+                    img_crop_from_upload = gr.Image(shape=(500, 500))
+                    gr.TextArea(
+                        interactive=False,
+                        label="license_text",
+                        info="license character data",
+                        lines=2,
+                    )                
 
             with gr.Row():
                 example_images = gr.Examples(
@@ -195,39 +103,22 @@ with demo:
 
             img_but = gr.Button("Detect")
 
-        with gr.TabItem("WebCam"):
-            with gr.Row():
-                web_input = gr.Image(
-                    source="webcam", type="pil", shape=(750, 750), streaming=True
-                )
-                img_output_from_webcam = gr.Image(shape=(750, 750))
-                with gr.Column():
-                    img_crop_from_webcam = gr.Image(shape=(750, 500))
-                    gr.TextArea(
-                        interactive=False,
-                        label="license_text",
-                        info="license character data",
-                        lines=2,
-                    )
-
-            cam_but = gr.Button("Detect")
-
-    url_but.click(
-        detect_objects,
+    cam_but.click(
+        detector.detect_objects,
         inputs=[options, url_input, img_input, web_input, slider_input],
-        outputs=[img_output_from_url],
+        outputs=[img_output_from_webcam, img_crop_from_webcam],
+        queue=True,
+    )
+    url_but.click(
+        detector.detect_objects,
+        inputs=[options, url_input, img_input, web_input, slider_input],
+        outputs=[img_output_from_url, img_crop_from_url],
         queue=True,
     )
     img_but.click(
-        detect_objects,
+        detector.detect_objects,
         inputs=[options, url_input, img_input, web_input, slider_input],
-        outputs=[img_output_from_upload],
-        queue=True,
-    )
-    cam_but.click(
-        detect_objects,
-        inputs=[options, url_input, img_input, web_input, slider_input],
-        outputs=[img_output_from_webcam, img_crop_from_webcam],
+        outputs=[img_output_from_upload, img_crop_from_upload],
         queue=True,
     )
 
---
+++ license_detector.py
@@ -0,0 +1,166 @@
+import io
+import matplotlib.pyplot as plt
+import requests, validators
+import torch
+from PIL import Image
+from transformers import (
+    AutoFeatureExtractor,
+    YolosForObjectDetection,
+    DetrForObjectDetection,
+)
+from FileManagerUtil import FileManagerUtil
+
+# colors for visualization
+COLORS = [
+    [0.000, 0.447, 0.741],
+    [0.850, 0.325, 0.098],
+    [0.929, 0.694, 0.125],
+    [0.494, 0.184, 0.556],
+    [0.466, 0.674, 0.188],
+    [0.301, 0.745, 0.933],
+]
+
+
+class license_detector:
+    _models = [
+        "nickmuchi/yolos-small-finetuned-license-plate-detection",
+        "nickmuchi/detr-resnet50-license-plate-detection",
+        "nickmuchi/yolos-small-rego-plates-detection",
+    ]
+
+    def __init__(self, model="") -> None:
+        siu = FileManagerUtil()
+        self.save_img_util = siu
+        self._default_model = "nickmuchi/yolos-small-finetuned-license-plate-detection"
+
+        if len(model) == 0:
+            self._model = self._default_model
+        else:
+            self._model = model
+
+    def getCurrentModel(self) -> str:
+        return self._model
+
+    def setModelName(self, model) -> None:
+        self._model = model
+
+    def verifyModel(self, model):
+        if len(model) == 0:
+            model = self._model
+        else:
+            self.setModelName(model)
+        return self.getCurrentModel()
+
+    def make_prediction(self, img, feature_extractor, model):
+        inputs = feature_extractor(img, return_tensors="pt")
+        outputs = model(**inputs)
+        img_size = torch.tensor([tuple(reversed(img.size))])
+        processed_outputs = feature_extractor.post_process(outputs, img_size)
+        return processed_outputs[0]
+
+    def fig2img(self, fig):
+        buf = io.BytesIO()
+        fig.savefig(buf)
+        buf.seek(0)
+        pil_img = Image.open(buf)
+        basewidth = 750
+        wpercent = basewidth / float(pil_img.size[0])
+        hsize = int((float(pil_img.size[1]) * float(wpercent)))
+        img = pil_img.resize((basewidth, hsize), Image.Resampling.LANCZOS)
+        return img
+
+    def visualize_prediction(self, img, output_dict, threshold=0.5, id2label=None):
+        keep = output_dict["scores"] > threshold
+        boxes = output_dict["boxes"][keep].tolist()
+        scores = output_dict["scores"][keep].tolist()
+        labels = output_dict["labels"][keep].tolist()
+
+        if len(boxes) > 0:
+            try:
+                crop_img = img.crop(*boxes)
+            except:
+                random_img = b"\xf8\xff\xb0\xbc\xd8]\xba\xdf0\xbd\xdeE\xfb\xff\xd1\xf1\xff\xbf\xb4\xd9p\xad\xd9F\xae\xd7U\xf2\xff\xd6\xdf\xff\xdc\xde\xff\xd2m\xa8L\xe0\xff\xc5\xe0\xff\xe1F\x9a\\I\x9e]E\x9bTD\x9aSK\x9fa1\xadO,\xa7L3\xaeT/\xa9R.\xa8Q"
+                crop_img = Image.frombytes("RGB", (5, 5), random_img)
+
+        if id2label is not None:
+            labels = [id2label[x] for x in labels]
+
+        plt.figure(figsize=(50, 50))
+        plt.imshow(img)
+        ax = plt.gca()
+        colors = COLORS * 100
+        for score, (xmin, ymin, xmax, ymax), label, color in zip(
+            scores, boxes, labels, colors
+        ):
+            if label == "license-plates":
+                ax.add_patch(
+                    plt.Rectangle(
+                        (xmin, ymin),
+                        xmax - xmin,
+                        ymax - ymin,
+                        fill=False,
+                        color=color,
+                        linewidth=10,
+                    )
+                )
+                ax.text(
+                    xmin,
+                    ymin,
+                    f"{label}: {score:0.2f}",
+                    fontsize=60,
+                    bbox=dict(facecolor="yellow", alpha=0.8),
+                )
+        plt.axis("off")
+
+        return self.fig2img(plt.gcf()), crop_img
+
+    """ def reduce_visual_to_license(self, img, output_dict, threshold=0.5, id2label=None):
+        keep = output_dict["scores"] > threshold
+        boxes = output_dict["boxes"][keep].tolist()
+        scores = output_dict["scores"][keep].tolist()
+        labels = output_dict["labels"][keep].tolist()
+
+        crop_img = img.crop(*boxes)
+        return crop_img """
+
+    def get_original_image(self, url_input):
+        if validators.url(url_input):
+            image = Image.open(requests.get(url_input, stream=True).raw)
+
+            return image
+
+    def detect_objects(
+        self, model_name, url_input, image_input, webcam_input, threshold
+    ):
+        model = self.verifyModel(model_name)
+
+        # Extract model and feature extractor
+        feature_extractor = AutoFeatureExtractor.from_pretrained(model_name)
+
+        if "yolos" in model_name:
+            model = YolosForObjectDetection.from_pretrained(model_name)
+        elif "detr" in model_name:
+            model = DetrForObjectDetection.from_pretrained(model_name)
+
+        if validators.url(url_input):
+            image = self.get_original_image(url_input)
+
+        elif image_input:
+            image = image_input
+
+        elif webcam_input:
+            image = webcam_input
+
+        # Make prediction
+        processed_outputs = self.make_prediction(image, feature_extractor, model)
+
+        # Visualize prediction
+        viz_img, crop_img = self.visualize_prediction(
+            image, processed_outputs, threshold, model.config.id2label
+        )
+
+        # save results
+        self.save_img_util.initialize_folders()
+        self.save_img_util.save_img_results(viz_img, crop_img)
+
+        return viz_img, crop_img
